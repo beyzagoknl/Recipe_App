5bb09feb030e81f4f92d572433c3e00ba814c80c
Beyza GOK <104020458+beyzagoknl@users.noreply.github.com>
2022-09-21 12:28:52 +0200

    Update recipePage.js

--- src/pages/recipePage.js
+++ src/pages/recipePage.js
@@ -9,7 +9,6 @@ const getMealRecipe = async (e) => {
   e.preventDefault();
   if (e.target.classList.contains("recipe-btn")) {
     let mealItem = e.target.parentElement.parentElement;
-    console.log(e.target.parentElement.parentElement);
     let URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`;
     try {
       const data = await fetchData(URL);
